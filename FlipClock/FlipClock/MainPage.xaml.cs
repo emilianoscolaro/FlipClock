e4b898b306047d5c5dbb529cc327ccec8d9d1b59
emiliano scolaro <escolaroo@gmail.com>
2019-11-28 19:29:29 -0300

    Agregar archivos de proyecto.

---
+++ FlipClock/FlipClock/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FlipClock
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await uno.RotateXTo(-90,400);
+            uno.RotationX = 0;
+        }
+    }
+}
